6a04d959e25c88bed8fd99e508b2dc79a8094413
芦厚祥 <houxiang.lu@inin88.com>
2018-02-01 23:52:49 +0800

    Feat: 实现byrobot version显示版本号的功能

---
+++ byrobot/__main__.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os
+import six
+import click
+
+CONTEXT_SETTINGS={
+    'default_map': {
+        'run': {
+        }
+    }
+}
+
+@click.group(context_settings=CONTEXT_SETTINGS)
+@click.option('-v', '--verbose', count=True)
+@click.help_option('-h', '--help')
+@click.pass_context
+def cli(ctx, verbose):
+    ctx.obj["VERBOSE"] = verbose
+
+def entry_point():
+    print("entry_point...")
+    inject_mod_commands()
+    
+    cli(obj={})
+    
+def inject_mod_commands():
+    from byrobot.utils.config import get_mod_conf
+    mod_config = get_mod_conf()
+    
+@cli.command()
+@click.option('-v', '--verbose', is_flag=True)
+def version(**kwargs):
+    """
+    Output Version Info
+    """
+    from byrobot import version_info
+    six.print_("Current Version: ", version_info)
+    
+if __name__ == '__main__':
+    entry_point()
